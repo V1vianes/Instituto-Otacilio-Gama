6f4035c9bae3769dcdfca61460cbbc87ca6a4ab9
Viviane <vivianesilvabatista@hotmail.com>
2025-10-24 17:41:10 -0300

    #

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const sanduiche = document.getElementById('sanduiche');
 const nav = document.querySelector('#nav ul');
 
-sanduiche.addEventListener('click', () => {
+['click', 'touchstart'].forEach(evt => {
+  sanduiche.addEventListener(evt, (e) => {
+    e.preventDefault(); 
     nav.classList.toggle('active');
     sanduiche.classList.toggle('open');
+  });
 });
